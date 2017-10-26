594d8a3ee4791f50c4f99f0d6ccb1a33b7c96491
jumperskirt <jsharonkim@gmail.com>
2017-10-26 15:52:41 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+//main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//db setup
+mongoose.connect('mongodb://localhost:auth/auth');
+
+//app setup (getting express working)
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+//server setup (getting express application to talk to outside waorld)
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on', port);
---
+++ server/models/user.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+//define our model
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  password: String
+});
+
+//on Save Hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password= hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  brypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
+
+//create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+//export the model
+module.exports = ModelClass;
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const bcrypt = require('bcrypt-nodejs');
+//
+// // Define our model
+// const userSchema = new Schema({
+//   email: { type: String, unique: true, lowercase: true },
+//   password: String
+// });
+//
+// // On Save Hook, encrypt password
+// // Before saving a model, run this function
+// userSchema.pre('save', function(next) {
+//   // get access to the user model
+//   const user = this;
+//
+//   // generate a salt then run callback
+//   bcrypt.genSalt(10, function(err, salt) {
+//     if (err) { return next(err); }
+//
+//     // hash (encrypt) our password using the salt
+//     bcrypt.hash(user.password, salt, null, function(err, hash) {
+//       if (err) { return next(err); }
+//
+//       // overwrite plain text password with encrypted password
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
+//
+// userSchema.methods.comparePassword = function(candidatePassword, callback) {
+//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//     if (err) { return callback(err); }
+//
+//     callback(null, isMatch);
+//   });
+// }
+//
+// // Create the model class
+// const ModelClass = mongoose.model('user', userSchema);
+//
+// // Export the model
+// module.exports = ModelClass;
